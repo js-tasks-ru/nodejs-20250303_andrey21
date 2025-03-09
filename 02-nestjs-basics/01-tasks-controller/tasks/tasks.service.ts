25b61ce4d9dcaa813fe4baf99a3a8b4b76c061c2
Andrew Gordienko <levanevskogo79@gmail.com>
2025-03-09 16:45:09 +0300

    add solution for 2-module 1-task"

--- 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
+++ 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
@@ -1,17 +1,38 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { Task } from "./task.model";
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
-  getAllTasks(): Task[] {}
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
 
-  getTaskById(id: string): Task {}
+  getTaskById(id: string): Task {
+    return this.tasks.find(task => task.id === id);
+  }
 
-  createTask(task: Task): Task {}
+  createTask(task: Task): Task {
+    const newTask = { ...task, id: String(this.tasks.length + 1) };
+    this.tasks.push(newTask);
+    return newTask;
+  }
 
-  updateTask(id: string, update: Task): Task {}
+  updateTask(id: string, update: Task): Task {
+    const taskIndex = this.tasks.findIndex(task => task.id === id);
+    if (taskIndex === -1) {
+      return null;
+    }
+    this.tasks[taskIndex] = {...this.tasks[taskIndex], ...update};
+    return this.tasks[taskIndex];
+  }
 
-  deleteTask(id: string): Task {}
+  deleteTask(id: string): Task {
+    const taskIndex = this.tasks.findIndex(task => task.id === id);
+    if (taskIndex === -1) {
+      return null
+    }
+    return this.tasks.splice(taskIndex, 1)[0]
+  }
 }
