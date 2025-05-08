714ae671157812b909cac1d704f0481f035c0b80
Andrew Gordienko <levanevskogo79@gmail.com>
2025-05-08 14:30:59 +0300

    add solution for 5-module

--- 05-db/01-crud-tasks/tasks/tasks.module.ts
+++ 05-db/01-crud-tasks/tasks/tasks.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
-import { TasksController } from "./tasks.controller";
+import { TaskController } from "./tasks.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Task } from "./entities/task.entity";
 
 @Module({
-  controllers: [TasksController],
+  imports: [TypeOrmModule.forFeature([Task])],
+  controllers: [TaskController],
   providers: [TasksService],
 })
 export class TasksModule {}
